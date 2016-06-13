353c081b684f36b5809e4acf1b88be85d3276db2
darron froese <darron@froese.org>
2016-06-13 11:25:48 -0600

    Update to 0.15.0

--- fpm-recipes/consul-template/recipe.rb
+++ fpm-recipes/consul-template/recipe.rb
@@ -1,13 +1,13 @@
 class ConsulTemplate < FPM::Cookery::Recipe
   name 'consul-template'
 
-  version '0.14.0'
+  version '0.15.0'
   revision '1'
   description 'Generic template rendering and notifications with Consul.'
 
   homepage 'https://github.com/hashicorp/consul-template'
   source "https://releases.hashicorp.com/consul-template/#{version}/consul-template_#{version}_linux_amd64.zip"
-  sha256 '7c70ea5f230a70c809333e75fdcff2f6f1e838f29cfb872e1420a63cdf7f3a78'
+  sha256 'b7561158d2074c3c68ff62ae6fc1eafe8db250894043382fb31f0c78150c513a'
 
   maintainer 'Darron Froese <darron@froese.org>'
   vendor 'octohost'
